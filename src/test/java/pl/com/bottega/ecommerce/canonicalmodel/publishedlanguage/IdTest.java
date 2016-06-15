68f811755e547ce7d6ccd56e1688cebc6679aa3e
Marek Wiacek <172234@edu.p.lodz.pl>
2016-06-16 00:14:35 +0200

    test1

---
+++ src/test/java/pl/com/bottega/ecommerce/canonicalmodel/publishedlanguage/IdTest.java
@@ -0,0 +1,22 @@
+package pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.hamcrest.CoreMatchers.*;
+
+public class IdTest {
+
+	Id id;
+	
+	@Test
+	public void userTest() {
+		id = new Id("Test");
+		boolean isValid = true;
+		
+		assertThat(id.equals(new Id("Test")), equalTo(isValid));
+		
+	}
+}
